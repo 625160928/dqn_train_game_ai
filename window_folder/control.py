cb4eb5c718565aa123104de2feed31261b645a28
10076 <1007628071@qq.com>
2021-06-11 00:16:17 +0800

    添加键盘控制方法，角色控制方法 当前存在的控制问题，控制命令发送时间的选择，发送时间如果阻塞会导致后续计算发生延迟

--- window_folder/control.py
+++ window_folder/control.py
@@ -5,141 +5,65 @@ import time
 
 import win32api
 import win32con
+import win32gui
 
-PostMessageW = windll.user32.PostMessageW
-MapVirtualKeyW = windll.user32.MapVirtualKeyW
-VkKeyScanA = windll.user32.VkKeyScanA
+class Player():
+    def __init__(self,player,window_name):
+        self.time_sleep=0.1
+        handle = windll.user32.FindWindowW(None,window_name)
+        self.w2hd=win32gui.FindWindowEx(handle,None,None,None)
 
-WM_KEYDOWN = 0x100
-WM_KEYUP = 0x101
+        if player=='red':
+            self.right=68
+            self.left=65
+            self.up=87
+            self.down=83
+        elif player=='blue':
+            self.right=39
+            self.left=37
+            self.up=38
+            self.down=40
+        else:
+            print("没有这个角色")
 
-# https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-VkCode = {
-    "back":  0x08,
-    "tab":  0x09,
-    "return":  0x0D,
-    "shift":  0x10,
-    "control":  0x11,
-    "menu":  0x12,
-    "pause":  0x13,
-    "capital":  0x14,
-    "escape":  0x1B,
-    "space":  0x20,
-    "end":  0x23,
-    "home":  0x24,
-    "left":  0x25,
-    "up":  0x26,
-    "right":  0x27,
-    "down":  0x28,
-    "print":  0x2A,
-    "snapshot":  0x2C,
-    "insert":  0x2D,
-    "delete":  0x2E,
-    "lwin":  0x5B,
-    "rwin":  0x5C,
-    "numpad0":  0x60,
-    "numpad1":  0x61,
-    "numpad2":  0x62,
-    "numpad3":  0x63,
-    "numpad4":  0x64,
-    "numpad5":  0x65,
-    "numpad6":  0x66,
-    "numpad7":  0x67,
-    "numpad8":  0x68,
-    "numpad9":  0x69,
-    "multiply":  0x6A,
-    "add":  0x6B,
-    "separator":  0x6C,
-    "subtract":  0x6D,
-    "decimal":  0x6E,
-    "divide":  0x6F,
-    "f1":  0x70,
-    "f2":  0x71,
-    "f3":  0x72,
-    "f4":  0x73,
-    "f5":  0x74,
-    "f6":  0x75,
-    "f7":  0x76,
-    "f8":  0x77,
-    "f9":  0x78,
-    "f10":  0x79,
-    "f11":  0x7A,
-    "f12":  0x7B,
-    "numlock":  0x90,
-    "scroll":  0x91,
-    "lshift":  0xA0,
-    "rshift":  0xA1,
-    "lcontrol":  0xA2,
-    "rcontrol":  0xA3,
-    "lmenu":  0xA4,
-    "rmenu":  0XA5
-}
+    #horizontal 三个离散值，1右走，0不动，-1左走
+    #jump 两个离散 0不跳，1跳
+    def move(self,horizontal,jump):
+        if horizontal==1:
+            press_key=self.right
+        elif horizontal==-1:
+            press_key=self.left
+        else:
+            press_key=None
+        win32gui.SetForegroundWindow(self.w2hd)
+        if press_key!=None:
+            win32api.keybd_event(press_key,0,0,0)
+        if jump==1:
+            win32api.keybd_event(self.up,0,0,0)
+        time.sleep(self.time_sleep)
+        if press_key!=None:
+            win32api.keybd_event(press_key,0,win32con.KEYEVENTF_KEYUP,0)
+        if jump==1:
+            win32api.keybd_event(self.up,0,win32con.KEYEVENTF_KEYUP,0)
 
 
-def get_virtual_keycode(key: str):
-    """根据按键名获取虚拟按键码
 
-    Args:
-        key (str): 按键名
-
-    Returns:
-        int: 虚拟按键码
-    """
-    if len(key) == 1 and key in string.printable:
-        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-vkkeyscana
-        return VkKeyScanA(ord(key)) & 0xff
-    else:
-        return VkCode[key]
-
-
-def key_down(handle: HWND, key: str):
-    """按下指定按键
-
-    Args:
-        handle (HWND): 窗口句柄
-        key (str): 按键名
-    """
-    vk_code = get_virtual_keycode(key)
-    scan_code = MapVirtualKeyW(vk_code, 0)
-    # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keydown
-    wparam = vk_code
-    lparam = (scan_code << 16) | 1
-    PostMessageW(handle, WM_KEYDOWN, wparam, lparam)
-
-
-def key_up(handle: HWND, key: str):
-    """放开指定按键
-
-    Args:
-        handle (HWND): 窗口句柄
-        key (str): 按键名
-    """
-    vk_code = get_virtual_keycode(key)
-    scan_code = MapVirtualKeyW(vk_code, 0)
-    # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keyup
-    wparam = vk_code
-    lparam = (scan_code << 16) | 0XC0000001
-    PostMessageW(handle, WM_KEYUP, wparam, lparam)
 
 
 if __name__ == "__main__":
-    # 需要和目标窗口同一权限，游戏窗口通常是管理员权限
-    import sys
-    # if not windll.shell32.IsUserAnAdmin():
-    #     # 不是管理员就提权
-    #     windll.shell32.ShellExecuteW(
-    #         None, "runas", sys.executable, __file__, None, 1)
 
-
-    import cv2
     window_name='FlashPlay'
-
     handle = windll.user32.FindWindowW(None,window_name)
-    print(handle)
-    # 控制角色向前移动两秒
-    key_down(handle, 'w')
-    time.sleep(2)
-    key_up(handle, 'w')
-
-    # win32api.keybd_event(13,0,0,0)
-    # win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0)
+    count=0
+    w2hd=win32gui.FindWindowEx(handle,None,None,None)
+    import time
+    start_time=time.time()
+    while(count<20):
+        count+=1
+        # win32gui.InSendMessage()
+        # win32gui.ReplyMessage(0)
+        win32gui.SetForegroundWindow(w2hd)
+        win32api.keybd_event(68,0,0,0)
+        time.sleep(1)
+        win32gui.SetForegroundWindow(w2hd)
+        win32api.keybd_event(68,0,win32con.KEYEVENTF_KEYUP,0)  #释放按键
--- window_folder/get_win.py
+++ window_folder/get_win.py
@@ -1,56 +1,58 @@
 from ctypes import windll, byref, c_ubyte
 from ctypes.wintypes import RECT, HWND
+
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-GetDC = windll.user32.GetDC
-CreateCompatibleDC = windll.gdi32.CreateCompatibleDC
-GetClientRect = windll.user32.GetClientRect
-CreateCompatibleBitmap = windll.gdi32.CreateCompatibleBitmap
-SelectObject = windll.gdi32.SelectObject
-BitBlt = windll.gdi32.BitBlt
-SRCCOPY = 0x00CC0020
-GetBitmapBits = windll.gdi32.GetBitmapBits
-DeleteObject = windll.gdi32.DeleteObject
-ReleaseDC = windll.user32.ReleaseDC
+class window_capture():
+    def __init__(self,window_name,reshape_height,reshape_width):
+        self.handle = windll.user32.FindWindowW(None, window_name)
+        self.reshape_height=reshape_height
+        self.reshape_width=reshape_width
 
-# 排除缩放干扰
-windll.user32.SetProcessDPIAware()
+    def capture(self):
+        """窗口客户区截图
 
-def capture(handle: HWND):
-    """窗口客户区截图
+        Args:
+            handle (HWND): 要截图的窗口句柄
 
-    Args:
-        handle (HWND): 要截图的窗口句柄
-
-    Returns:
-        numpy.ndarray: 截图数据
-    """
-    # 获取窗口客户区的大小
-    r = RECT()
-    GetClientRect(handle, byref(r))
-    width, height = r.right, r.bottom
-    # 开始截图
-    dc = GetDC(handle)
-    cdc = CreateCompatibleDC(dc)
-    bitmap = CreateCompatibleBitmap(dc, width, height)
-    SelectObject(cdc, bitmap)
-    BitBlt(cdc, 0, 0, width, height, dc, 0, 0, SRCCOPY)
-    # 截图是BGRA排列，因此总元素个数需要乘以4
-    total_bytes = width*height*4
-    buffer = bytearray(total_bytes)
-    byte_array = c_ubyte*total_bytes
-    GetBitmapBits(bitmap, total_bytes, byte_array.from_buffer(buffer))
-    DeleteObject(bitmap)
-    DeleteObject(cdc)
-    ReleaseDC(handle, dc)
-    # 返回截图数据为numpy.ndarray
-    return np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 4)
+        Returns:
+            numpy.ndarray: 截图数据
+        """
+        # 获取窗口客户区的大小
+        r = RECT()
+        windll.user32.SetProcessDPIAware()
+        windll.user32.GetClientRect(self.handle, byref(r))
+        width, height = r.right, r.bottom
+        # 开始截图
+        dc = windll.user32.GetDC(self.handle)
+        cdc = windll.gdi32.CreateCompatibleDC(dc)
+        bitmap = windll.gdi32.CreateCompatibleBitmap(dc, width, height)
+        windll.gdi32.SelectObject(cdc, bitmap)
+        windll.gdi32.BitBlt(cdc, 0, 0, width, height, dc, 0, 0, 0x00CC0020)
+        # 截图是BGRA排列，因此总元素个数需要乘以4
+        total_bytes = width*height*4
+        buffer = bytearray(total_bytes)
+        byte_array = c_ubyte*total_bytes
+        windll.gdi32.GetBitmapBits(bitmap, total_bytes, byte_array.from_buffer(buffer))
+        windll.gdi32.DeleteObject(bitmap)
+        windll.gdi32.DeleteObject(cdc)
+        windll.user32.ReleaseDC(self.handle, dc)
+        # 返回截图数据为numpy.ndarray
+        return np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 4)
+        # return np.frombuffer(buffer, dtype=np.uint8).reshape(self.reshape_height, self.reshape_width, 4)
 
 if __name__ == "__main__":
     window_name='FlashPlay'
-    handle = windll.user32.FindWindowW(None, window_name)
-    image = capture(handle)
-    print(image)
-    cv2.imshow("Capture Test", image)
-    cv2.waitKey()
+    win_c=window_capture(window_name,600,800)
+    count=0
+    while count<300:
+        image =  win_c.capture()
+        print(image.shape,image[950][1250])
+        # print(image)
+        # cv2.imshow("Capture Test", image)
+        # cv2.waitKey()
+        plt.imshow(image)
+        plt.pause(0.01)
+        count+=1
